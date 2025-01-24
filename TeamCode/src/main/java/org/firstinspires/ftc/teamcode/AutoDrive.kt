a31ecf6c71d2a1625fd8a7ef725141668bbe8f62
FTC22289 <yecrobotics22289@gmail.com>
2025-01-24 11:55:56 -0800

    pushbot 2.0: 2 samples maybe?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.kt
@@ -12,6 +12,7 @@ class AutoDrive : LinearOpMode() {
         var hwmap = Hardware(this)
         waitForStart()
 
+        //drives forward to push middle sample
         while (opModeIsActive() && (runtime < 0.25+3) && (runtime > 0.25)) {
             telemetry.addData("Forward", "Leg 1: %4.1f S Elapsed", runtime)
             telemetry.update()
@@ -19,14 +20,36 @@ class AutoDrive : LinearOpMode() {
             hwmap.driveRobot(0.5,0.075,0.0)
         }
 
-        while (opModeIsActive() && (runtime < 0.25+3+3) && (runtime > 0.25+3)) {
+        resetRuntime()
+
+        //drives back to starting position
+        while (opModeIsActive() && (runtime < 0.25+3) && (runtime > 0.25)) {
             telemetry.addData("Backwards", "Leg 2: %4.1f S Elapsed", runtime)
             telemetry.update()
 
-            hwmap.driveRobot(-0.5,-0.075,0.0)
+            hwmap.driveRobot(-0.5,0.075,0.0)
         }
 
+        resetRuntime()
+
+        while (opModeIsActive() && (runtime < 0.25+1.5) && (runtime > 0.25)) {
+            telemetry.addData("Forward left", "Leg 1: %4.1f S Elapsed", runtime)
+            telemetry.update()
+
+            hwmap.driveRobot(0.5,-0.1,0.0)
+        }
+
+        while (opModeIsActive() && (runtime < 0.25+1.5+1.5) && (runtime > 0.25+1.5)) {
+            telemetry.addData("Forward right", "Leg 1: %4.1f S Elapsed", runtime)
+            telemetry.update()
+
+            hwmap.driveRobot(0.5,0.1,0.0)
+        }
     }
+
+
+
+
 
 
 }
